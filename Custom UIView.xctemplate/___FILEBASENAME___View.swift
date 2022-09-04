415583512212ba42e43ed52642341b675aa6fa32
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2022-09-04 19:33:41 +0200

    Fixed typo

--- Custom UIView.xctemplate/___FILEBASENAME___View.swift
+++ Custom UIView.xctemplate/___FILEBASENAME___View.swift
@@ -4,7 +4,7 @@
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //
-// Copyright (c) ___YEAR___ Oscar R. Garrucho. All rights reserved.
+//  Copyright (c) ___YEAR___ Oscar R. Garrucho. All rights reserved.
 //  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
 //
 
